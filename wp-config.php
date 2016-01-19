74e5bd7ecb5645bce33f7c96e369d14b63605c68
Jon Busby <jon.busby@wearetwogether.com>
2016-01-19 21:45:33 +0000

    allowing for different values of some S3 constants in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'AMAZON_KEY', getenv('AWS_ACCESS_KEY_ID') );
 define( 'AMAZON_SECRET', getenv('AWS_SECRET_ACCESS_KEY') );
 define( 'AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID') );
 define( 'AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY') );
-define( 'AS3CF_BUCKET', getenv('AMAZON_BUCKET'))
+define( 'AS3CF_BUCKET', getenv('AMAZON_BUCKET'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
